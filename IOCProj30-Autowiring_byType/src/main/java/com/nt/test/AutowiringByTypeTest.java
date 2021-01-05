87b7d38fa9961a6428dcc42264194af7c1540e9f
Sourav Kumar Nayak <souravkumarnayak408@gmail.com>
2021-01-05 09:07:51 +0530

    byType mode of auto wiring and solving ambiguity problems

---
+++ IOCProj30-Autowiring_byType/src/main/java/com/nt/test/AutowiringByTypeTest.java
@@ -0,0 +1,25 @@
+package com.nt.test;
+
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+
+import com.nt.comp.Flipkart;
+
+public final class AutowiringByTypeTest {
+
+	public static void main(String[] args) {
+		DefaultListableBeanFactory factory=null;
+		XmlBeanDefinitionReader reader=null;
+		Flipkart fpkt=null;
+		//create bean factory object
+		factory=new DefaultListableBeanFactory();
+		//create reader class object
+		reader=new XmlBeanDefinitionReader(factory);
+		//locate and load spring bean configuration file
+		reader.loadBeanDefinitions("com/nt/cfgs/applicationContext.xml");
+		//get target bean object
+		fpkt=factory.getBean("fpkt",Flipkart.class);
+		//use target class business method
+		System.out.println(fpkt.shopping(new String[]{"watch","shoe","dress"},new float[] {27003.32f,12343.00f,22000}));
+	}//main
+}//class
