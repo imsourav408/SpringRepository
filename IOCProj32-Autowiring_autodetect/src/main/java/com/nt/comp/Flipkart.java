996ffe43eb0cc2f223e6f76a6498aedb8e15eabe
Sourav Kumar Nayak <souravkumarnayak408@gmail.com>
2021-01-05 09:36:06 +0530

    autoDetect mode of auto wiring and solving ambiguity problems.

---
+++ IOCProj32-Autowiring_autodetect/src/main/java/com/nt/comp/Flipkart.java
@@ -0,0 +1,40 @@
+package com.nt.comp;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public final class Flipkart {
+	//property
+	private Courier courier;
+
+	public void setCourier(Courier courier){
+		this.courier = courier;
+	}
+	
+	public Flipkart(Courier courier) {
+		System.out.println("Flipkart.Flipkart(-)");
+		this.courier = courier;
+	}
+	
+	public Flipkart() {
+		System.out.println("Flipkart.Flipkart()");
+	}
+	 
+	//business method
+	public String shopping(String[] items,float[] prices) {
+		float billAmt=0.0f;
+		int oid=0;
+		String msg=null;
+		Random random=null;
+		//calculate bill amount
+		for(float price:prices)
+			billAmt+=price;
+		//generate order id as random number
+		random=new Random();
+		oid=random.nextInt(100000);
+		//use courier service for delivering the product
+		msg=courier.deliver(oid);
+		return Arrays.toString(items)+" are purchased having prices "+Arrays.toString(prices)+" with bill amount "+billAmt+"---"+msg;
+	}//shopping
+	
+}//class
