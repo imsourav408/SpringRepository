99cd80b95b37f7501d156f82ba567b17b53c2a55
Sourav Kumar Nayak <souravkumarnayak408@gmail.com>
2021-01-20 11:34:51 +0530

    Real time use of nested bean IOC container.

---
+++ IOCProj46-MiniProj2-NestedContainer/src/main/java/com/nt/controller/MainController.java
@@ -0,0 +1,45 @@
+package com.nt.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
+
+import com.nt.dto.EmployeeDTO;
+import com.nt.service.EmployeeMgmtService;
+import com.nt.vo.EmployeeVO;
+
+public final class MainController {
+	
+	private EmployeeMgmtService service=null;
+
+	public MainController(EmployeeMgmtService service) {
+		System.out.println("MainController.1-param constructor");
+		this.service = service;
+	}
+	
+	public List<EmployeeVO> gatherEmpsByDesgs(String desg1,String desg2,String desg3)throws Exception{
+		List<EmployeeVO> listVO=null;
+		List<EmployeeDTO> listDTO=null;
+		EmployeeVO vo=null;
+		//use service
+		listDTO=service.fetchEmpsByDesgs(desg1, desg2, desg3);
+		//convert listDTO to listVO
+		listVO=new ArrayList<EmployeeVO>();
+		for(EmployeeDTO dto:listDTO) {
+			//convert each DTO to each VO
+			vo=new EmployeeVO();
+			BeanUtils.copyProperties(dto, vo);
+			vo.setSerialNo(String.valueOf(dto.getSerialNo()));
+            vo.setDeptno(String.valueOf(dto.getDeptno()));
+            vo.setSal(String.valueOf(dto.getSal()));
+            vo.setEmpno(String.valueOf(dto.getEmpno()));
+            vo.setComm(String.valueOf(dto.getComm()));
+            vo.setMgr(String.valueOf(dto.getMgr()));
+            vo.setHiredate(String.valueOf(dto.getHiredate()));
+			//add each VO to listVO
+            listVO.add(vo);
+		}
+		return listVO;
+	}
+}
---
+++ IOCProj46-MiniProj2-NestedContainer/src/main/java/com/nt/service/EmployeeMgmtServiceImpl.java
@@ -0,0 +1,50 @@
+package com.nt.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
+
+import com.nt.bo.EmployeeBO;
+import com.nt.dao.EmployeeDAO;
+import com.nt.dto.EmployeeDTO;
+
+public final class EmployeeMgmtServiceImpl implements EmployeeMgmtService {
+
+	private EmployeeDAO dao;
+	
+	
+	public EmployeeMgmtServiceImpl(EmployeeDAO dao) {
+		System.out.println("EmployeeMgmtServiceImpl.1-param constructor");
+		this.dao = dao;
+	}
+
+
+	@Override
+	public List<EmployeeDTO> fetchEmpsByDesgs(String desg1, String desg2, String desg3) throws Exception {
+		System.out.println("EmployeeMgmtServiceImpl.fetchEmpsByDesgs()");
+		List<EmployeeDTO> listDTO=null;
+		List<EmployeeBO> listBO=null;
+		EmployeeDTO dto=null;
+		//convert designation to upper case (b.logic)
+		desg1=desg1.toUpperCase();
+		desg2=desg2.toUpperCase();
+		desg3=desg3.toUpperCase();
+		//use DAO
+		listBO=dao.getEmployees(desg1, desg2, desg3);
+		//convert listBO to listDTO
+		listDTO=new ArrayList<EmployeeDTO>();
+		for(EmployeeBO bo:listBO) {
+			dto=new EmployeeDTO();
+			// copies data from one java bean object to another bean object when they have matching properties names,types
+			BeanUtils.copyProperties(bo, dto);
+			//set round figure salary
+			dto.setSal((float)Math.round(bo.getSal()));
+			//set serial number
+			dto.setSerialNo(listDTO.size()+1);
+			//add each dto
+			listDTO.add(dto);
+		}
+		return listDTO;
+	}
+}
